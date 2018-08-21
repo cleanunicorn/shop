0f07b8d8270784476fcc8834e3c83b9c965f9639
Daniel Luca <lucadanielcostin@gmail.com>
2018-08-21 21:34:45 +0300

    Fix testing transaction status

--- test/shop.js
+++ test/shop.js
@@ -45,7 +45,7 @@ contract('Shop', (accounts) => {
             );
     
             // Tx was successful
-            assert.isTrue(productCreated.receipt.status == "0x01", "Transaction was not successful");
+            assert.isTrue(BigNumber(productCreated.receipt.status).toString() == 1, "Transaction was not successful");
     
             // Event was emitted correctly
             assert.equal(
